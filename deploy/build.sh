548668d5d41d7064b62ee750b24ce908659fe538
이수빈 <dktnqls0624@naver.com>
2025-01-18 21:59:31 +0900

    update: build script

--- deploy/build.sh
+++ deploy/build.sh
@@ -32,7 +32,7 @@ build_and_push_image() {
     local registry="$3"
 
     echo "Docker 이미지를 빌드합니다: ${registry}/${image_name}:${tag}"
-    docker build -t "${image_name}:${tag}" ./server
+    docker build -t "${image_name}:${tag}" ../server
 
     echo "Docker 이미지를 푸시합니다: ${registry}/${image_name}:${tag}"
     docker tag "${image_name}:${tag}" "${registry}/${image_name}:${tag}"
